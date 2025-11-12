65d0a523ade9d25d8ce88051e0313599b3af22a5
DIVYA K <divyakdiv10@gmail.com>
2025-11-13 00:38:11 +0530

    Time: 1 ms (98.71%), Space: 42.7 MB (6.8%) - LeetHub

---
+++ 4051-remove-zeros-in-decimal-representation/4051-remove-zeros-in-decimal-representation.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long removeZeros(long n) {
+      long result = 0;
+      long place = 1;
+
+      while(n>0){
+        long digit = n%10;
+        n=n/10;
+
+        if(digit!=0){
+            result = result + digit * place;
+            place *=10;
+        }
+      }
+          return result;
+       
+    }
+}
