bef9ad83c57387f76c8c65ab971a605ceb0fb27e
DIVYA K <divyakdiv10@gmail.com>
2025-11-16 01:58:06 +0530

    Time: 2 ms (97.65%), Space: 43.2 MB (18.18%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+          
+            if (ch == '(' || ch == '{' || ch == '[') {
+                sb.append(ch);
+            } else {
+               
+                if (sb.length() == 0) return false;
+
+                char last = sb.charAt(sb.length() - 1); 
+                if ((ch == ')' && last == '(') ||
+                    (ch == '}' && last == '{') ||
+                    (ch == ']' && last == '[')) {
+
+                    sb.deleteCharAt(sb.length() - 1); // pop
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return sb.length() == 0;
+    }
+}
