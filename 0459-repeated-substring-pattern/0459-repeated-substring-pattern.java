86ba0cf835ccf797c93db1ae2d21ea5569d09497
DIVYA K <divyakdiv10@gmail.com>
2026-01-21 23:09:21 +0530

    Time: 75 ms (50.74%), Space: 47.2 MB (7.67%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        
+    String ss = s + s;
+    return ss.substring(1, ss.length()-1).contains(s);
+        
+    }
+}
