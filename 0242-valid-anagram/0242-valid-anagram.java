b783e00576cafcdb35320ad5ec7968227de0d02e
DIVYA K <divyakdiv10@gmail.com>
2026-01-26 23:12:03 +0530

    Time: 3 ms (90.57%), Space: 44.5 MB (65.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,13 +1,10 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
+        int freq[] = new int[26];
         int len1 = s.length();
         int len2 = t.length();
-                    
-        if(len1!=len2) {
-            return false;
-        }
-        int[] freq = new int[26];
+
+        if(len1!=len2) return false;
 
         for(int i=0; i<len1; i++){
             freq[s.charAt(i)-'a']++;
@@ -16,6 +13,9 @@ class Solution {
             freq[t.charAt(i)-'a']--;
             if(freq[t.charAt(i)-'a']<0) return false;
         }
-        return true;
+
+        return true;                  
+    
+        
     }
 }
