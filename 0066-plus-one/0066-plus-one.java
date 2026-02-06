193260cd0578739d4cf48cae4416b6b178db93e8
DIVYA K <divyakdiv10@gmail.com>
2026-02-06 20:24:07 +0530

    Time: 0 ms (100%), Space: 43.6 MB (35.81%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,19 +1,19 @@
-public class Solution {
+class Solution {
     public int[] plusOne(int[] digits) {
-       
-        for (int i = digits.length - 1; i >= 0; i--) {
-           
-            if (digits[i] < 9) {
-                digits[i]++;         
-                return digits;      
-            }
-           
-            digits[i] = 0;
-        }
 
-       
-        int[] result = new int[digits.length + 1];
-        result[0] = 1; 
-        return result;
+      for(int i=digits.length-1; i>=0; i--){
+        if(digits[i]<9){
+            digits[i]++;
+            return digits;
+        }
+        digits[i]=0;
+      }
+      
+      int[] results = new int[digits.length + 1];
+      results[0]=1;
+      return results;
+
+    
     }
+
 }
