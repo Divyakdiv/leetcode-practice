ef7eb20185940dc5f5833aa107241ab551f2b6ae
DIVYA K <divyakdiv10@gmail.com>
2026-09-02 12:58:56 +0530

    Time: 5 ms (82.23%), Space: 45.9 MB (71.96%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0)
-        {
-          return false;
-        }
+     if(x<0){
+        return false;
+     }
+     int num = x;
+     int rev = 0;
 
-        int origin = x;
-        int rev = 0;
-
-        while(x>0)
-        {
-          rev = rev*10 + x%10;
-          x = x/10;
-        }
-        return origin==rev;
-        
-    }
+     while(x>0){
+        int digit = x % 10;
+        rev = rev * 10 + digit;
+        x = x/10;
+     }
+     if(num==rev){
+        return true;
+     }else{
+        return false;
+     }
+  
+}
 }
